a79da5da111f389230e8d0d8d6056dedaaade9f4
NavidFarahmand <NavidFarahmand75@gmail.com>
2016-01-15 23:28:32 +0330

    optimize execute module

--- ExecutModule.v
+++ ExecutModule.v
@@ -24,60 +24,62 @@ module Module2;
     end
 endmodule
 
-module ExecutionModule(inNumbers, show, reset, store, update, outNumbers, err);
-  input show, reset, store, update;
+module ExecutionModule(inNumbers, show, reset, store, update, outNumbers, errIn, errOut);
+  input show, reset, store, update, errIn;
   input [13:0] inNumbers;
-  output reg[34:0] outNumbers;
-  output reg err;
+  output reg[16:0] outNumbers;
+  output errOut;
+  
+  reg overflow;
+  
+  assign errOut = errIn | overflow;
   
   reg [16:0] total, temp, temp2;
   
   initial
     begin
       
-      $monitor("total : %d, \t temp: %d, \t temp2: %d, err: %b, inNumbers: %d ", total, temp, temp2, err, inNumbers);
+      //$monitor("total : %d, \t temp: %d, \t temp2: %d, errIn: %b, overflow: %b, errOut: %b, inNumbers: %d ", total, temp, temp2, errIn, overflow, errOut, inNumbers);
       outNumbers = 0;
       total = 0;
       temp = 0;
       temp2 = 0;
-      err = 1'b0;
-      $display("total is : %d, \t temp: %d, \t temp2: %d, err: %b, inNumbers: %d ", total, temp, temp2, err, inNumbers);
+      overflow = 1'b0;
     end
   
   always @(posedge show or negedge show)
     begin
-      $display("show");
       temp2 = total + temp;
       total = temp2;
       temp = 0;
       temp2 = 0;
-      //total should be appeared in the output
+      outNumbers = total;
     end
   
   always @(posedge reset or negedge reset)
     begin
-      $display("reset");
       total = 0;
       temp = 0;
-      temp2 = 0;  
+      temp2 = 0; 
+      outNumbers = 0; 
     end
     
   always @(posedge store or negedge store)
     begin
-      $display("store");
       temp2 = temp + total;
       total = temp2;
       temp = 0;
       temp = inNumbers * 1;
+      outNumbers = total;
     end
   
   always @(posedge update or negedge update)
     begin
-      $display("update");
-      if( inNumbers * temp > 99999)
-        err = 1'b1;
+      if( inNumbers * temp > 99999 || inNumbers > 99 )
+        overflow = 1'b1;
       else begin
         temp = inNumbers * temp;
+        outNumbers = inNumbers;
       end
     end
   
